3fe1794aaf5f5af67c9969bd3123567636c4bfde
NeoHeathen <aiprofitvault@gmail.com>
2026-09-01 00:31:59 -0700

    Sync og:url with current route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installOgUrlSync } from "./ogUrl";
 import "./styles.css";
+
+installOgUrlSync();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
